02faa0c14d3345982a926eff7e633ec36d252127
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-10-27 19:41:01 +0100

    feat: implement pq-distance computation

---
+++ src/pqDistance.ts
@@ -0,0 +1,29 @@
+import {PQGramProfile, PQTree} from "./PQGramProfile";
+import {requirePositiveInteger} from "./util";
+
+export interface PQOpts {
+    p: number;
+    q: number;
+}
+
+/**
+ * Computes the pq-gram distance of the two given trees. Uses p=2 and q=3 by default.
+ *
+ * @param t1 A tree
+ * @param t2 Another tree
+ * @param opts Custom p and q values, optional
+ */
+export function pqDistance<T, U>(t1: Tree<T>, t2: Tree<U>, opts: Partial<PQOpts> = {}): number {
+    const {p, q} = {
+        p: 2,
+        q: 3,
+        ...opts,
+    };
+
+    requirePositiveInteger(p, q);
+
+    const p1 = PQGramProfile.of(t1, p, q);
+    const p2 = PQGramProfile.of(t2, p, q);
+
+    return 1 - 2 * (p1.intersect(p2) / (p1.length + p2.length));
+}
